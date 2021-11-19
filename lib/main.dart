e0fdc1de028543a13d221d543eb999137efae0ff
spainbarca <spain.barcelona.1999@gmail.com>
2021-11-18 23:45:38 -0500

    Se cambió el título de la App

--- lib/main.dart
+++ lib/main.dart
@@ -57,7 +57,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the button this many times:',
+              'Dando clicks desde Flutter Web:',
             ),
             Text(
               '$_counter',
